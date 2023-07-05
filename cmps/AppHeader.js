5c2b3530d4766ef64f5ceb26fbe3d07b74e3b869
Shay Zigdon <zigshay@gmail.com>
2023-07-05 11:26:59 +0300

    add route in header to 2 apps

--- cmps/AppHeader.js
+++ cmps/AppHeader.js
@@ -4,7 +4,9 @@ export default {
             <h1>AppSus</h1>
             <nav>
                 <router-link to="/">Home</router-link> | 
-                <router-link to="/about">About</router-link>
+                <router-link to="/about">About</router-link>|
+                <RouterLink to="/keep">Keep</RouterLink> |
+                <RouterLink to="/mail">Mail</RouterLink> |
             </nav>
         </header>
     `,
--- routes.js
+++ routes.js
@@ -1,5 +1,7 @@
 import HomePage from './views/HomePage.js'
 import AboutUs from './views/AboutUs.js'
+import NoteIndex from './apps/keep/pages/NoteIndex.js'
+import MailIndex from './apps/mail/pages/MailIndex.js'
 
 const { createRouter, createWebHashHistory } = VueRouter
 
@@ -14,6 +16,14 @@ const routerOptions = {
 			path: '/about',
 			component: AboutUs,
 		},
+		{
+            path: '/keep',
+            component: NoteIndex
+        },
+		{
+            path: '/mail',
+            component: MailIndex
+        },
 	],
 }
 
