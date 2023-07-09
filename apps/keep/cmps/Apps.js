88474cb35c5ea341c862d8fce564644d003127a2
Shay Zigdon <zigshay@gmail.com>
2023-07-09 08:32:12 +0300

    img github

--- apps/keep/cmps/Apps.js
+++ apps/keep/cmps/Apps.js
@@ -4,14 +4,14 @@ export default {
         <div className="apps">
             <div className="keep">
                 <RouterLink to="/keep">
-               <img src="../../assets/img/keep.png" alt="" />
+               <img src="././assets/img/keep.png" alt="" />
                <h2>Keep</h2>
                </RouterLink> 
             </div>
             
             <div className="mail">
                 <RouterLink to="/mail">
-                <img src="../../assets/img/gmail.png" alt="" />
+                <img src="././assets/img/gmail.png" alt="" />
                 <h2>Mail</h2>
                 </RouterLink> 
             </div>
